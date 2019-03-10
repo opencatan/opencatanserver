646467f487120ed7c3bb8b421bb47ee5bb6ebdb2
thabaptiser <baptiste.vauthey@gmail.com>
2019-03-10 14:20:15 -0700

    forgot my offer file :(

---
+++ offer.py
@@ -0,0 +1,5 @@
+class Offer:
+    def __init__(self, player_maker, resources_from, resources_to):
+        self.player_maker = player_maker
+        self.resources_from = resources_from
+        self.resources_to = resources_to
